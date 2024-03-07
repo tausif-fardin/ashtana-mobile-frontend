be117e2a766ee0ca949c6351d490e295637bbd6a
Tausif Fardin <tausiffardin4@gmail.com>
2024-03-07 15:17:55 +0600

    design update

--- src/components/productmini.js
+++ src/components/productmini.js
@@ -1,136 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import {
-  Linking,
-  Image,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import { useEffect, useState } from "react";
+import { Image, View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { AntDesign, FontAwesome6 } from "@expo/vector-icons";
 import s from "../styles/mainStyle.js";
 
 const ProductMini = (props) => {
-  useEffect(() => {}, []);
+  const [isLiked, setIsLiked] = useState(false);
+  const [optionColors, setOptionColors] = useState([]);
+
+  useEffect(() => {
+    // Generate random colors for options
+    const colors = Array.from({ length: 4 }, () => getRandomColor());
+    setOptionColors(colors);
+  }, []);
+
+  const getRandomColor = () => {
+    return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)})`;
+  };
+
+  const renderTitle = () => {
+    const { title } = props.product;
+    return (
+      <Text style={styles.productTitle}>
+        {title.length > 20 ? `${title.substring(0, 18)}..` : title}
+      </Text>
+    );
+  };
+
+  const toggleLike = () => {
+    setIsLiked((prevIsLiked) => !prevIsLiked);
+  };
+
   return (
-    <View
-      style={[
-        {
-          width: props.width || "auto",
-          height: props.height || "auto",
-          minHeight: 100,
-        },
-        !props.vertical && {
-          marginLeft: 10,
-          marginRight: 10,
-          padding: 4,
-        },
-        props.productStyle,
-      ]}
-    >
-      <View style={{ flex: 1 }}>
-        <View style={[s.textCenter]}>
-          <Image
-            source={{ uri: props.product.image }}
-            style={{
-              width: "100%",
-              height: props.productimgheight || 120,
-              resizeMode: props.productimgresizemode || "contain",
-            }}
+    <View style={[styles.container, props.productStyle]}>
+      <View style={styles.imageContainer}>
+        <TouchableOpacity style={styles.vrIcon}>
+          <FontAwesome6 name="vr-cardboard" size={24} color="black" />
+        </TouchableOpacity>
+        <Image source={{ uri: props.product.image }} style={styles.image} />
+        <TouchableOpacity style={styles.heartIcon} onPress={toggleLike}>
+          <AntDesign
+            name={isLiked ? "heart" : "hearto"}
+            size={24}
+            color="black"
           />
-        </View>
+        </TouchableOpacity>
       </View>
-      <View style={{ flex: 1, marginTop: 10 }}>
-        <Text style={[props.productTitleStyle || s.f15]}>
-          {props.product.title.length > 20
-            ? props.product.title.substring(0, 18) + ".."
-            : props.product.title}
-        </Text>
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 4,
-            marginTop: 2,
-            marginBottom: 2,
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "red",
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-              borderWidth: 2,
-              borderRadius: 14,
-            }}
-          ></View>
-          <View
-            style={{
-              backgroundColor: "green",
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-
-              borderWidth: 2,
-              borderRadius: 14,
-            }}
-          ></View>
-          <View
-            style={{
-              backgroundColor: "yellow",
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-
-              borderWidth: 2,
-              borderRadius: 14,
-            }}
-          ></View>
-          <View
-            style={{
-              backgroundColor: "black",
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-              borderWidth: 2,
-              borderRadius: 14,
-            }}
-          ></View>
+      <View style={styles.detailsContainer}>
+        {renderTitle()}
+        <View style={styles.optionsContainer}>
+          {optionColors.map((color, index) => (
+            <View
+              key={index}
+              style={[styles.option, { backgroundColor: color }]}
+            />
+          ))}
         </View>
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 4,
-            marginTop: 2,
-            marginBottom: 2,
-          }}
-        >
+        <View style={styles.sizeContainer}>
           <Text style={styles.size}>M</Text>
           <Text style={styles.size}>L</Text>
           <Text style={styles.size}>XL</Text>
           <Text style={styles.size}>XXL</Text>
         </View>
-        <View
-          style={{
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: 2,
-            marginBottom: 2,
-          }}
-        >
-          <Text style={{ fontWeight: "500", fontSize: 16 }}>
-            {props.product.price || "140$"}
-          </Text>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>{props.product.price || "140$"}</Text>
           <Text>({props.product.discount || "30%"} Discount)</Text>
         </View>
-
         <TouchableOpacity style={styles.btn}>
-          <Text style={{ fontWeight: "500", color: "white" }}>Add to Bag</Text>
+          <Text style={styles.btnText}>Add to Bag</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -138,31 +77,111 @@ const ProductMini = (props) => {
 };
 
 const styles = StyleSheet.create({
-  btn: {
-    width: "100%",
-    height: 35,
-    borderRadius: 14,
-    backgroundColor: "#000000",
+  container: {
+    width: "auto",
+    height: "auto",
+    minHeight: 100,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 4,
+  },
+  imageContainer: {
+    width: 150,
+    height: 200,
+    position: "relative",
+    borderWidth: 2,
+    padding: 10,
+    paddingVertical: 16,
     justifyContent: "center",
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 1.2,
-    elevation: 3,
-    marginTop: 4,
+    textAlign: "center",
   },
-  card: {
-    border: 1,
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  vrIcon: {
+    position: "absolute",
+    top: "50%",
+    left: -15,
+    transform: [{ translateY: -6 }],
+    backgroundColor: "transparent",
+    borderRadius: 6,
+    paddingHorizontal: 2,
+    borderWidth: 1,
+    borderColor: "black",
+    zIndex: 1,
+  },
+  heartIcon: {
+    position: "absolute",
+    top: 5,
+    right: 2,
+    backgroundColor: "transparent",
+    zIndex: 1,
+  },
+  productTitle: {
+    fontSize: 16,
+  },
+  detailsContainer: {
+    flex: 1,
+    marginTop: 10,
+  },
+  optionsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 4,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  option: {
+    backgroundColor: "black",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderWidth: 2,
+    borderRadius: 14,
+  },
+  sizeContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 4,
+    marginTop: 2,
+    marginBottom: 2,
   },
   size: {
     fontWeight: "500",
     paddingHorizontal: 6,
     borderWidth: 1,
     borderRadius: 10,
+  },
+  priceContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  btn: {
+    alignSelf: "center",
+    paddingHorizontal: 6,
+    height: 30,
+    borderWidth: 2,
+    borderRadius: 14,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 4,
+  },
+  btnText: {
+    fontWeight: "500",
+    color: "black",
   },
 });
 
